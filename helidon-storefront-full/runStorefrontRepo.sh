e7d6bb280b2f926a2d1a5ee62a0cc17d6bb211ff
tim_graves <28924492+atimgraves@users.noreply.github.com>
2022-01-17 17:38:56 +0000

    force the run repo to use 0.0.1 by default

--- helidon-storefront-full/runStorefrontRepo.sh
+++ helidon-storefront-full/runStorefrontRepo.sh
@@ -8,4 +8,4 @@ export CONFSECURE=$RUNDIR/confsecure
 export ZIPKINIP=`docker inspect --type container -f '{{.NetworkSettings.IPAddress}}' zipkin`
 export STOCKIP=`docker inspect --type container -f '{{.NetworkSettings.IPAddress}}' stockmanager`
 echo zipkin ip $ZIPKINIP
-docker run  --add-host zipkin:$ZIPKINIP --add-host stockmanager:$STOCKIP --publish  8080:8080 --publish  9080:9080 --rm  --volume $CONF:/$CONTAINER_DIR/conf --volume $CONFSECURE:/$CONTAINER_DIR/confsecure --name storefront  $REPO
+docker run  --add-host zipkin:$ZIPKINIP --add-host stockmanager:$STOCKIP --publish  8080:8080 --publish  9080:9080 --rm  --volume $CONF:/$CONTAINER_DIR/conf --volume $CONFSECURE:/$CONTAINER_DIR/confsecure --name storefront  $REPO:0.0.1
